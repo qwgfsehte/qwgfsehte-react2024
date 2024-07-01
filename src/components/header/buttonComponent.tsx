76fd20d44439a313bb306277fe8a82902b7aaa22
Viktoria Rashchepkina <vika.rashchepkina.04@bk.ru>
2024-07-01 17:37:35 +0500

    feat: add axios

--- src/components/header/buttonComponent.tsx
+++ src/components/header/buttonComponent.tsx
@@ -1,8 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 
 class SearchButton extends React.Component {
+  handleClick() {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }
+
   render(): React.ReactNode {
-    return <button className="search-form__button"></button>;
+    return (
+      <button
+        className="search-form__button"
+        onClick={this.handleClick}
+      ></button>
+    );
   }
 }
 
