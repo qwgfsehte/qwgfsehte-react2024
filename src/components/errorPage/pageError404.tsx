2fb2130ebf15ba9e0f562e1d4acebf95cd2cd34b
Viktoria Rashchepkina <vika.rashchepkina.04@bk.ru>
2024-07-13 22:33:35 +0500

    feat: update error page 404 and add tests

--- src/components/errorPage/pageError404.tsx
+++ src/components/errorPage/pageError404.tsx
@@ -1,16 +1,22 @@
-import { useRouteError } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './pageError404.scss';
 
 export default function ErrorPage404() {
-  const error = useRouteError();
-  console.error(error);
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{}</i>
-      </p>
+    <div id="error-page" className="error-page">
+      <div className="error-page__error-container">
+        <h1 style={{ fontSize: '45px' }}>404</h1>
+        <h2>Not Found</h2>
+        <img
+          src="./src/assets/imgs/img-for-error-page.png"
+          alt="pokemon-error"
+          className="error-page__img"
+        />
+        <p>This page doesn’t exist.</p>
+        <Link className="error-page__button" to="/">
+          Go to home
+        </Link>
+      </div>
     </div>
   );
 }
