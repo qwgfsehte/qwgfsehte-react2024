556ef30540cd5b72717ec8033464ac47409e7cfb
Viktoria Rashchepkina <vika.rashchepkina.04@bk.ru>
2024-08-11 13:18:43 +0500

    feat: update display loading indicator

--- src/components/body/pokemonsList/pokemonsList.tsx
+++ src/components/body/pokemonsList/pokemonsList.tsx
@@ -2,8 +2,7 @@ import './pokemonList.scss';
 import { Pokeball } from './pokeball';
 import { AppProps, PokemonCardInfo } from '../../../interfaces/interface';
 import { filterPokemons } from '../../hooks/useFilterPokemons';
-import { Link } from '@remix-run/react';
-import { useCookie } from '../../hooks/useCookies';
+import { Link, useNavigation } from '@remix-run/react';
 import { ChangeEvent, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { ModalWindow } from '../flyout/flyout';
@@ -14,12 +13,9 @@ function PokemonsList({
   allPokemons,
   currentPage,
 }: AppProps): React.ReactElement {
-  const [storedValue] = useCookie('searchValueInput');
+  const [storedValue, setStoredValue] = useState('');
   const [items, setItems] = useState<string[]>([]);
-  const [filteredPokemons, setFilteredPokemons] = useState<
-    PokemonCardInfo[] | { name: string; url: string }[][]
-  >([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const navigation = useNavigation();
 
   useEffect(() => {
     const savedItems = Cookies.get('selectedPokemons');
@@ -27,6 +23,22 @@ function PokemonsList({
       setItems(JSON.parse(savedItems));
     }
   }, []);
+
+  useEffect(() => {
+    const savedValue = Cookies.get('searchValueInput');
+    if (savedValue) {
+      setStoredValue(savedValue);
+    }
+
+    const interval = setInterval(() => {
+      const newValue = Cookies.get('searchValueInput');
+      if (newValue !== storedValue) {
+        setStoredValue(newValue as string);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [storedValue]);
 
   const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { id, checked } = event.target;
@@ -51,64 +63,57 @@ function PokemonsList({
     });
   };
 
-  useEffect(() => {
-    async function loadPokemons() {
-      setLoading(true);
-      const result = filterPokemons(allPokemons, currentPage, storedValue);
-      setFilteredPokemons(result);
-      setLoading(false);
-    }
-    loadPokemons();
-  }, [allPokemons, currentPage, storedValue]);
-
-  if (loading) {
-    return <Loading />;
-  }
-
-  if (filteredPokemons.length === 0) {
-    return <ErrorMessage />;
-  }
-
   return (
-    <div className={'pokemons-list'}>
-      {filteredPokemons.map((pokemon, index) =>
-        pokemon ? (
-          <div key={index} className="card-container">
-            <input
-              data-testid={`checkbox-${(pokemon as PokemonCardInfo).name}-${index}`}
-              id={
-                (pokemon as PokemonCardInfo).name +
-                ' - ' +
-                (pokemon as PokemonCardInfo).url
-              }
-              className="pokemon-select"
-              type="checkbox"
-              onChange={handleCheckboxChange}
-              checked={items?.includes(
-                (pokemon as PokemonCardInfo).name +
-                  ' - ' +
-                  (pokemon as PokemonCardInfo).url
-              )}
-            ></input>
-            <Link
-              key={index}
-              className="card"
-              to={`/search/page/${currentPage}/details/${(pokemon as PokemonCardInfo).name}`}
-            >
-              <Pokeball />
-              <h3 className="pokemon-name">
-                {(pokemon as PokemonCardInfo).name}
-              </h3>
-            </Link>
+    <>
+      {navigation.state === 'loading' && <Loading />}
+      {navigation.state !== 'loading' &&
+        filterPokemons(allPokemons, currentPage, storedValue).length === 0 && (
+          <ErrorMessage />
+        )}
+      {navigation.state !== 'loading' &&
+        filterPokemons(allPokemons, currentPage, storedValue).length !== 0 && (
+          <div className={'pokemons-list'}>
+            {filterPokemons(allPokemons, currentPage, storedValue).map(
+              (pokemon, index) =>
+                pokemon ? (
+                  <div key={index} className="card-container">
+                    <input
+                      data-testid={`checkbox-${(pokemon as PokemonCardInfo).name}-${index}`}
+                      id={
+                        (pokemon as PokemonCardInfo).name +
+                        ' - ' +
+                        (pokemon as PokemonCardInfo).url
+                      }
+                      className="pokemon-select"
+                      type="checkbox"
+                      onChange={handleCheckboxChange}
+                      checked={items?.includes(
+                        (pokemon as PokemonCardInfo).name +
+                          ' - ' +
+                          (pokemon as PokemonCardInfo).url
+                      )}
+                    ></input>
+                    <Link
+                      key={index}
+                      className="card"
+                      to={`/search/page/${currentPage}/details/${(pokemon as PokemonCardInfo).name}`}
+                    >
+                      <Pokeball />
+                      <h3 className="pokemon-name">
+                        {(pokemon as PokemonCardInfo).name}
+                      </h3>
+                    </Link>
+                  </div>
+                ) : (
+                  <div key={index} className="placeholder">
+                    Pokemon not found
+                  </div>
+                )
+            )}
+            <ModalWindow selectedItems={items} clearItems={clearItems} />
           </div>
-        ) : (
-          <div key={index} className="placeholder">
-            Pokemon not found
-          </div>
-        )
-      )}
-      <ModalWindow selectedItems={items} clearItems={clearItems} />
-    </div>
+        )}
+    </>
   );
 }
 
--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,25 +1,39 @@
 import { useEffect, useState } from 'react';
 import './pagination.scss';
 import { filterPokemons } from '../hooks/useFilterPokemons';
-import { Link } from '@remix-run/react';
+import { Link, useNavigation } from '@remix-run/react';
 import { AppProps, PokemonCardInfo } from '../../interfaces/interface';
-import { useCookie } from '../hooks/useCookies';
+import Cookies from 'js-cookie';
 
 const PAGES_PER_GROUP = 10;
 
 export function Pagination({ allPokemons, currentPage }: AppProps) {
   const [currentGroup, setCurrentGroup] = useState(0);
-  const [storedValue] = useCookie('searchValueInput');
+  const [storedValue, setStoredValue] = useState('');
   const [filteredPokemons, setFilteredPokemons] = useState<
     PokemonCardInfo[] | { name: string; url: string }[][]
   >([]);
+  const navigation = useNavigation();
 
   useEffect(() => {
-    async function loadPokemons() {
-      const result = filterPokemons(allPokemons, currentPage, storedValue);
-      setFilteredPokemons(result);
+    const savedValue = Cookies.get('searchValueInput');
+    if (savedValue) {
+      setStoredValue(savedValue);
     }
-    loadPokemons();
+
+    const interval = setInterval(() => {
+      const newValue = Cookies.get('searchValueInput');
+      if (newValue !== storedValue) {
+        setStoredValue(newValue as string);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [storedValue]);
+
+  useEffect(() => {
+    const result = filterPokemons(allPokemons, currentPage, storedValue, true);
+    setFilteredPokemons(result);
   }, [allPokemons, currentPage, storedValue]);
 
   const startPage = currentGroup * PAGES_PER_GROUP;
@@ -28,42 +42,46 @@ export function Pagination({ allPokemons, currentPage }: AppProps) {
     filteredPokemons.length
   );
 
-  if (filteredPokemons.length === 0) {
-    return;
-  }
-
   return (
-    <section className="pagination-container">
-      <button
-        disabled={currentGroup === 0}
-        className="pagination__button button-left"
-        data-testid="button-left"
-        onClick={() => setCurrentGroup(currentGroup - 1)}
-      ></button>
-      <div className="pagination">
-        {filteredPokemons.slice(startPage, endPage).map((_, index) => {
-          const pageIndex = startPage + index + 1;
-          return (
-            <Link
-              key={pageIndex}
-              className={`pagination__item ${pageIndex === Number(currentPage) ? 'pagination__item_active' : ''}`}
-              to={`/search/page/${pageIndex}`}
-            >
-              {pageIndex}
-            </Link>
-          );
-        })}
-      </div>
-      <button
-        disabled={
-          currentGroup === filteredPokemons.slice(startPage, endPage).length
-        }
-        className="pagination__button button-right"
-        data-testid="button-right"
-        onClick={() => {
-          setCurrentGroup(currentGroup + 1);
-        }}
-      ></button>
-    </section>
+    <>
+      {navigation.state === 'loading' ||
+        (filteredPokemons.length === 0 && <div></div>)}
+      {navigation.state !== 'loading' && filteredPokemons.length !== 0 && (
+        <section className="pagination-container">
+          <button
+            disabled={currentGroup === 0}
+            className="pagination__button button-left"
+            data-testid="button-left"
+            onClick={() => setCurrentGroup(currentGroup - 1)}
+          ></button>
+          <div className="pagination">
+            {filterPokemons(allPokemons, currentPage, storedValue)
+              .slice(startPage, endPage)
+              .map((_, index) => {
+                const pageIndex = startPage + index + 1;
+                return (
+                  <Link
+                    key={pageIndex}
+                    className={`pagination__item ${pageIndex === Number(currentPage) ? 'pagination__item_active' : ''}`}
+                    to={`/search/page/${pageIndex}`}
+                  >
+                    {pageIndex}
+                  </Link>
+                );
+              })}
+          </div>
+          <button
+            disabled={
+              currentGroup === filteredPokemons.slice(startPage, endPage).length
+            }
+            className="pagination__button button-right"
+            data-testid="button-right"
+            onClick={() => {
+              setCurrentGroup(currentGroup + 1);
+            }}
+          ></button>
+        </section>
+      )}
+    </>
   );
 }
